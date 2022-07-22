005b1bec36fd22fa550e8bbd4765b8ef746aac50
EuilimChoi <91925895+EuilimChoi@users.noreply.github.com>
2022-07-22 18:16:24 +0900

    비동기 크롤링 로직 작성 중

--- crawling/views.py
+++ crawling/views.py
@@ -4,6 +4,8 @@ from rest_framework.response import Response
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
+import time
+import asyncio
 
 # Create your views here.
 
@@ -11,17 +13,47 @@ from selenium.webdriver.common.by import By
 class CrawlingView (APIView):
     def post (self, request):
         driver = webdriver.Chrome("/Users/choeuilim/Downloads/chromedriver")
-        target = request.data["data"][0]["place_url"]
-        driver.get(target)
-        driver.implicitly_wait(5)
+        result = []
 
-        driver.execute_script('window.scrollTo(0, 200)')
-        # tt = driver.find_element("xpath",'//*[@id="mArticle"]/div[3]/div[2]/div/a[1]')
-        # ss = driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/div[2]/div[5]/div[2]/ul')
-        driver.implicitly_wait(10)
+        async def crwaling(target):
+            shopinfo = {"shopName" : target["place_name"],
+                    "shoppic" :[],
+                    "menu": []
+                    }
 
-        html = driver.page_source
-        soup = BeautifulSoup(html, 'html.parser')
-        # print(ss)
+            driver.get(target["place_url"])
+            await asyncio.sleep(3)
 
-        return Response(str(soup))
+            html = driver.page_source
+            soup = BeautifulSoup(html, 'html.parser')
+
+        
+            pic = soup.find('ul',{'class':'list_photo photo_type5'})
+            pics = pic.find_all('a')
+            for p in pics :
+                pp = p.get_attribute_list('style')
+                picURL = pp[0][24:55] + "C320x320/" + pp[0][64:-1]
+                shopinfo["shoppic"].append(picURL)
+
+
+            try:
+                menu = soup.find('ul',{'class':'list_menu'})
+                menus = menu.find_all('li')
+                for m in menus:
+                    menu = m.find("span",{"class":"loss_word"}).get_text()
+                    price = m.find("em",{"class":"price_menu"}).get_text()
+                    menuSet = [menu, price[4:]]
+                    shopinfo["menu"].append(menuSet)
+            except:
+                shopinfo["menu"].append(["메뉴 정보 없음","가격 정보 없음"])
+
+            print(shopinfo)
+            result.append(shopinfo)
+
+        shops = request.data["data"]
+
+
+        for shop in shops:
+            asyncio.run(crwaling(shop))
+
+        return Response(result)
